5edc86ebc6e6e83999818f87b49955d837a7b436
jloiola <jeff.loiola@gmail.com>
2020-05-24 22:00:36 -0400

    mocked gmail layout

--- packages/rule-ui-stencil/src/components/rui-app/rui-app.tsx
+++ packages/rule-ui-stencil/src/components/rui-app/rui-app.tsx
@@ -8,13 +8,46 @@ import { Component, h } from "@stencil/core";
 export class App {
   render() {
     return (
-      <rui-layout direction='column'>
-        <rui-box name="app-bar" slot="north">
-          Header bar
+      <rui-layout>
+
+        <rui-box name="app-bar" slot="north" height={64}>
+          <rui-layout>
+            <rui-box slot="west" width={256}>
+              Header name thing
+            </rui-box>
+            <rui-box fill={true} >
+              search bar thing
+            </rui-box>
+
+            <rui-box slot="east" width={256}>
+              user thing
+            </rui-box>
+          </rui-layout>
         </rui-box>
-        <rui-box name="main" fill={true} >
-          this is the main content
+
+        <rui-box name="app-bar" slot="west" width={256}>
+          Sidebad west
         </rui-box>
+
+        <rui-box name="main" fill={true} gutter={0}>
+
+          <rui-layout>
+
+            <rui-box name="app-bar" slot="north" height={48}>
+            toooool bar
+            </rui-box>
+
+            <rui-box name="item list" slot="west" width={344}>
+              Items list
+            </rui-box>
+            <rui-box name="main" fill={true}>
+
+            </rui-box>
+            </rui-layout>
+
+        </rui-box>
+
+
       </rui-layout>
     );
   }
